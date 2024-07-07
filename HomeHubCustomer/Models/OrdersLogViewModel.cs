0a1d241c27ff77efa8000104b68aa25c998ef9a5
tutorial <charlizepaw@gmail.com>
2024-07-07 15:53:06 +0800

    Added Provider Order Logs

---
+++ HomeHubCustomer/Models/OrdersLogViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeHub.App.Models
+{
+    public class OrdersLogViewModel
+    {
+        public string LogId { get; set; } = null!;
+
+        public string OrderId { get; set; } = null!;
+
+        public DateTime OrderDate { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string BusinessId { get; set; } = null!;
+
+        public string Item { get; set; } = null!;
+
+        public int Qty { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Status { get; set; } = null!;
+    }
+}
