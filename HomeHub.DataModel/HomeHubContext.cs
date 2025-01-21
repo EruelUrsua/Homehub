ca66707c249e8313ed5b5d4ea59680d3a4f8c95c
EruelUrsua <eruelursua@gmail.com>
2025-01-21 17:51:21 +0800

    fixed some bugs

--- HomeHub.DataModel/HomeHubContext.cs
+++ HomeHub.DataModel/HomeHubContext.cs
@@ -39,13 +39,13 @@ public partial class HomeHubContext : DbContext
             // optionsBuilder.UseSqlServer("Server=DESKTOP-TRU0264\\SQLEXPRESS;Database=HomeHub;Integrated Security=SSPI;TrustServerCertificate=true;");
 
 
-       //     optionsBuilder.UseSqlServer("Server=DESKTOP-HGGKL34\\SQLEXPRESS;" +
-       //   "Database=HomeHub; Integrated Security=SSPI;" +
-       //   "TrustServerCertificate=true");
-
-    optionsBuilder.UseSqlServer("Server=DESKTOP-JJNUTRM\\MSSQL2022;" +
+            optionsBuilder.UseSqlServer("Server=DESKTOP-HGGKL34\\SQLEXPRESS;" +
           "Database=HomeHub; Integrated Security=SSPI;" +
           "TrustServerCertificate=true");
+
+    //optionsBuilder.UseSqlServer("Server=DESKTOP-JJNUTRM\\MSSQL2022;" +
+    //      "Database=HomeHub; Integrated Security=SSPI;" +
+    //      "TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
