79ebc6701cbbd828ae913e6f1fb6e2382ee74a88
EruelUrsua <eruelursua@gmail.com>
2025-01-22 15:56:36 +0800

    added design nuget

--- HomeHub.App/obj/Debug/net8.0/HomeHub.App.AssemblyInfo.cs
+++ HomeHub.App/obj/Debug/net8.0/HomeHub.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeHub.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca66707c249e8313ed5b5d4ea59680d3a4f8c95c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7143490cbf52729458c31da2a256e81eeba7e481")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeHub.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeHub.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
