46055bb775a75d4dc682cc6c79aadbf6677f571e
EruelUrsua <eruelursua@gmail.com>
2025-01-29 17:59:02 +0800

    fix

--- HomeHub.App/obj/Debug/net8.0/HomeHub.App.AssemblyInfo.cs
+++ HomeHub.App/obj/Debug/net8.0/HomeHub.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeHub.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f88514670bb211b2c6ce757de195477b3762a779")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d3a850283639b7d8453b03fc02e2a76e3270df")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeHub.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeHub.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
