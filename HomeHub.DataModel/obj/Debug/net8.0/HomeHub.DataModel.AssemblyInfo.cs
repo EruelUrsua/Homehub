0e0a93205009f35655c341f88256e589aa16fd53
NickolRaven <nickolpascual@gmail.com>
2025-03-11 19:27:32 +0800

    fixed Register UI and View Orders buttons

--- HomeHub.DataModel/obj/Debug/net8.0/HomeHub.DataModel.AssemblyInfo.cs
+++ HomeHub.DataModel/obj/Debug/net8.0/HomeHub.DataModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeHub.DataModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852f30c941e61b9132754debd8064ef0c9adcf64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4211e361c9a6031b82afd80622c37797fd2a67ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeHub.DataModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeHub.DataModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
