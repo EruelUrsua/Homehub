5cc1dda67a3259f62227d4ce398b40f45e43385c
EruelUrsua <eruelursua@gmail.com>
2025-04-15 18:55:16 +0800

    working dashboard

---
+++ HomeHub.App/Models/ADashboardVM.cs
@@ -0,0 +1,19 @@
+﻿namespace HomeHub.App.Models
+{
+    public class ADashboardVM
+    {
+       public int customers { get; set; }
+
+        public int providers { get; set; }
+
+        //public int transactions { get; set; }
+
+        public int userRestricted { get; set; }
+
+        public int userVerified { get; set; }
+
+        public int userReview { get; set; }
+
+        public int totalUser { get; set; }
+    }
+}
--- HomeHub.DataModel/obj/Debug/net8.0/HomeHub.DataModel.AssemblyInfo.cs
+++ HomeHub.DataModel/obj/Debug/net8.0/HomeHub.DataModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeHub.DataModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f443b7068cb8cbc33e1804a0aec330665236f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2334bd0d6161ec54fe4355193a8cd09dca24c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeHub.DataModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeHub.DataModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
